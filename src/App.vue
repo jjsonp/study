fcf3adfbec2205ad9674214b94849aa5b15d0147
jjsonp <3303332443@qq.com>
2020-03-16 15:41:19 +0800

    fir

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <header>{{msg}}</header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg:"ssssssssss"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
